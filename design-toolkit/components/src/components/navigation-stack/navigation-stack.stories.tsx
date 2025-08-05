ba42012c0bd6400d8473fac45482609a7ec23483
Brenna Switzer <brenna.switzer@hypergiant.com>
2025-08-05 09:26:47 -0700

    fix: lint

--- design-toolkit/components/src/components/navigation-stack/navigation-stack.stories.tsx
+++ design-toolkit/components/src/components/navigation-stack/navigation-stack.stories.tsx
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 import { ChevronLeft } from '@accelint/icons';
 import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from '../button';
@@ -16,10 +28,10 @@ const meta: Meta<typeof NavigationStack> = {
           used in drawers and modals to navigate between different views. The NavigationStack component
           provides a way to push and pop views from the stack and it also provides a way to clear all
           views back to the original view.
-        `
-      }
-    }
-  }
+        `,
+      },
+    },
+  },
 };
 
 export default meta;
