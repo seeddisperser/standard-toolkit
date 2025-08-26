442158a4249e471afb73c29259148ba454b9dda8
Ray Knight <array.knight+github@gmail.com>
2025-08-26 14:18:51 -0700

    refactor: 💡 types and shared config

--- design-toolkit/components/src/lib/utils.ts
+++ design-toolkit/components/src/lib/utils.ts
@@ -13,109 +13,122 @@
 import { type ClassValue, clsx } from 'clsx';
 import type { ForwardedRef } from 'react';
 import type { ContextValue } from 'react-aria-components';
-import { extendTailwindMerge, validators } from 'tailwind-merge';
-import { createTV } from 'tailwind-variants';
-import type { TVConfig } from 'tailwind-variants/dist/config.js';
+import {
+  type ClassNameValue,
+  extendTailwindMerge,
+  validators,
+} from 'tailwind-merge';
+import {
+  type TVCompoundSlots,
+  type TVCompoundVariants,
+  type TVDefaultVariants,
+  type TVProps,
+  type TVReturnType,
+  type TVVariants,
+  createTV,
+} from 'tailwind-variants';
 
 type AdditionalClassGroupIds = 'icon' | 'icon-size' | 'fg';
 
-export const twMerge = extendTailwindMerge<AdditionalClassGroupIds>({
-  extend: {
-    classGroups: {
-      icon: [{ icon: ['', validators.isAny] }],
-      fg: [{ fg: ['', validators.isAny] }],
-    },
-    conflictingClassGroups: {
-      fg: ['icon', 'text-color'],
-    },
-    theme: {
-      color: [
-        'current',
-        'surface-default',
-        'surface-raised',
-        'surface-overlay',
-        'transparent-dark',
-        'transparent-light',
-        'interactive-default',
-        'interactive-hover-light',
-        'interactive-hover-dark',
-        'interactive-disabled',
-        'static-light',
-        'static-dark',
-        'interactive',
-        'interactive-hover',
-        'highlight-bold',
-        'highlight-hover',
-        'highlight-subtle',
-        'info-bold',
-        'info-hover',
-        'info-subtle',
-        'advisory-bold',
-        'advisory-hover',
-        'advisory-subtle',
-        'normal-bold',
-        'normal-hover',
-        'normal-subtle',
-        'serious-bold',
-        'serious-hover',
-        'serious-subtle',
-        'critical-bold',
-        'critical-hover',
-        'critical-subtle',
-        'default-light',
-        'default-dark',
-        'inverse-dark',
-        'inverse-light',
-        'disabled',
-        'highlight',
-        'info',
-        'advisory',
-        'normal',
-        'serious',
-        'critical',
-        'classification-missing',
-        'classification-unclass',
-        'classification-cui',
-        'classification-confidential',
-        'classification-secret',
-        'classification-top-secret',
-      ],
-      font: ['primary', 'display'],
-      text: [
-        'header-xxl',
-        'header-xl',
-        'header-l',
-        'header-m',
-        'header-s',
-        'header-xs',
-        'body-xl',
-        'body-l',
-        'body-m',
-        'body-s',
-        'body-xs',
-        'body-xxs',
-        'button-xl',
-        'button-l',
-        'button-m',
-        'button-s',
-        'button-xs',
-      ],
-      radius: ['none', 'small', 'medium', 'large', 'round'],
-      shadow: ['elevation-default', 'elevation-overlay', 'elevation-raised'],
-      spacing: [
-        'none',
-        '0',
-        'xxs',
-        'xs',
-        's',
-        'm',
-        'l',
-        'xl',
-        'xxl',
-        'oversized',
-      ],
-    },
+const twMergeConfig = {
+  classGroups: {
+    icon: [{ icon: ['', validators.isAny] }],
+    fg: [{ fg: ['', validators.isAny] }],
   },
+  conflictingClassGroups: {
+    fg: ['icon', 'text-color'],
+  },
+  theme: {
+    color: [
+      'current',
+      'surface-default',
+      'surface-raised',
+      'surface-overlay',
+      'transparent-dark',
+      'transparent-light',
+      'interactive-default',
+      'interactive-hover-light',
+      'interactive-hover-dark',
+      'interactive-disabled',
+      'static-light',
+      'static-dark',
+      'interactive',
+      'interactive-hover',
+      'highlight-bold',
+      'highlight-hover',
+      'highlight-subtle',
+      'info-bold',
+      'info-hover',
+      'info-subtle',
+      'advisory-bold',
+      'advisory-hover',
+      'advisory-subtle',
+      'normal-bold',
+      'normal-hover',
+      'normal-subtle',
+      'serious-bold',
+      'serious-hover',
+      'serious-subtle',
+      'critical-bold',
+      'critical-hover',
+      'critical-subtle',
+      'default-light',
+      'default-dark',
+      'inverse-dark',
+      'inverse-light',
+      'disabled',
+      'highlight',
+      'info',
+      'advisory',
+      'normal',
+      'serious',
+      'critical',
+      'classification-missing',
+      'classification-unclass',
+      'classification-cui',
+      'classification-confidential',
+      'classification-secret',
+      'classification-top-secret',
+    ],
+    font: ['primary', 'display'],
+    text: [
+      'header-xxl',
+      'header-xl',
+      'header-l',
+      'header-m',
+      'header-s',
+      'header-xs',
+      'body-xl',
+      'body-l',
+      'body-m',
+      'body-s',
+      'body-xs',
+      'body-xxs',
+      'button-xl',
+      'button-l',
+      'button-m',
+      'button-s',
+      'button-xs',
+    ],
+    radius: ['none', 'small', 'medium', 'large', 'round'],
+    shadow: ['elevation-default', 'elevation-overlay', 'elevation-raised'],
+    spacing: [
+      'none',
+      '0',
+      'xxs',
+      'xs',
+      's',
+      'm',
+      'l',
+      'xl',
+      'xxl',
+      'oversized',
+    ],
+  },
+} as const; // TODO: satisfies ?
+
+export const twMerge = extendTailwindMerge<AdditionalClassGroupIds>({
+  extend: twMergeConfig,
 });
 
 export function cn(...inputs: ClassValue[]) {
@@ -123,13 +136,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 const PREFIX_REGEX = /((?:[^:]*:)+)([^:]+)/;
-
-function pushToKey(map: Record<string, string[]>, key: string, value: string) {
-  if (!(key in map)) {
-    map[key] = [];
-  }
-  map[key]?.push(value);
-}
 
 export function mergeVariants(className: string) {
   if (!className) {
@@ -141,7 +147,10 @@ export function mergeVariants(className: string) {
 
   for (const cls of className.split(' ')) {
     const [, prefix, className] = PREFIX_REGEX.exec(cls) ?? ['', 'BASE', cls];
-    pushToKey(prefixMap, prefix, className);
+
+    prefixMap[prefix] = prefixMap[prefix] ?? [];
+
+    prefixMap[prefix].push(className);
   }
 
   const finalClassNames = [];
@@ -162,132 +171,69 @@ export function mergeVariants(className: string) {
 }
 
 const _tv = createTV({
-  twMergeConfig: {
-    extend: {
-      classGroups: {
-        icon: [{ icon: ['', validators.isAny] }],
-        fg: [{ fg: ['', validators.isAny] }],
-      },
-      conflictingClassGroups: {
-        fg: ['icon', 'text-color'],
-      },
-      theme: {
-        color: [
-          'current',
-          'surface-default',
-          'surface-raised',
-          'surface-overlay',
-          'transparent-dark',
-          'transparent-light',
-          'interactive-default',
-          'interactive-hover-light',
-          'interactive-hover-dark',
-          'interactive-disabled',
-          'static-light',
-          'static-dark',
-          'interactive',
-          'interactive-hover',
-          'highlight-bold',
-          'highlight-hover',
-          'highlight-subtle',
-          'info-bold',
-          'info-hover',
-          'info-subtle',
-          'advisory-bold',
-          'advisory-hover',
-          'advisory-subtle',
-          'normal-bold',
-          'normal-hover',
-          'normal-subtle',
-          'serious-bold',
-          'serious-hover',
-          'serious-subtle',
-          'critical-bold',
-          'critical-hover',
-          'critical-subtle',
-          'default-light',
-          'default-dark',
-          'inverse-dark',
-          'inverse-light',
-          'disabled',
-          'highlight',
-          'info',
-          'advisory',
-          'normal',
-          'serious',
-          'critical',
-          'classification-missing',
-          'classification-unclass',
-          'classification-cui',
-          'classification-confidential',
-          'classification-secret',
-          'classification-top-secret',
-        ],
-        font: ['primary', 'display'],
-        text: [
-          'header-xxl',
-          'header-xl',
-          'header-l',
-          'header-m',
-          'header-s',
-          'header-xs',
-          'body-xl',
-          'body-l',
-          'body-m',
-          'body-s',
-          'body-xs',
-          'body-xxs',
-          'button-xl',
-          'button-l',
-          'button-m',
-          'button-s',
-          'button-xs',
-        ],
-        radius: ['none', 'small', 'medium', 'large', 'round'],
-        shadow: ['elevation-default', 'elevation-overlay', 'elevation-raised'],
-        spacing: [
-          'none',
-          '0',
-          'xxs',
-          'xs',
-          's',
-          'm',
-          'l',
-          'xl',
-          'xxl',
-          'oversized',
-        ],
-      },
-    },
-  },
+  twMergeConfig,
 });
 
-type TailwindVariantFunction = (
-  // biome-ignore lint/suspicious/noExplicitAny: hard to match TV types.
-  ...props: any[]
-) => string;
+// Copied from Tailwind Variants due to not being exported
+type TVSlots = Record<string, ClassNameValue> | undefined;
 
-// biome-ignore lint/suspicious/noExplicitAny: hard to match TV types.
-export const tv = (options: any, config?: TVConfig) => {
-  const result = _tv(options, config);
+export function tv<
+  V extends TVVariants<S, B, EV>,
+  // biome-ignore lint/style/useNamingConvention: Keeping consistent with OG implementation within TV
+  CV extends TVCompoundVariants<V, S, B, EV, ES>,
+  // biome-ignore lint/style/useNamingConvention: Keeping consistent with OG implementation within TV
+  DV extends TVDefaultVariants<V, S, EV, ES>,
+  B extends ClassNameValue = undefined,
+  S extends TVSlots = undefined,
+  // @ts-expect-error
+  E extends TVReturnType = TVReturnType<
+    V,
+    S,
+    B,
+    // @ts-expect-error Keeping consistent with OG implementation within TV
+    EV extends undefined ? object : EV,
+    // @ts-expect-error Keeping consistent with OG implementation within TV
+    ES extends undefined ? object : ES
+  >,
+  // biome-ignore lint/style/useNamingConvention: Keeping consistent with OG implementation within TV
+  EV extends TVVariants<ES, B, E['variants'], ES> = E['variants'],
+  // biome-ignore lint/style/useNamingConvention: Keeping consistent with OG implementation within TV
+  ES extends TVSlots = E['slots'] extends TVSlots ? E['slots'] : undefined,
+>(options: {
+  extend?: E;
+  base?: B;
+  slots?: S;
+  variants?: V;
+  compoundVariants?: CV;
+  compoundSlots?: TVCompoundSlots<V, S, B>;
+  defaultVariants?: DV;
+}): TVReturnType<V, S, B, EV, ES, E> {
+  const configuredStateMachine = _tv<V, CV, DV, B, S, E, EV, ES>(options);
+  const wrappedStateMachine = ((props) => {
+    const classNamesOrSlots = configuredStateMachine(props);
 
-  // biome-ignore lint/suspicious/noExplicitAny: hard to match TV types.
-  const wrapper = (...internalProps: any[]) => {
-    const resultReturn = result(...internalProps);
+    return typeof classNamesOrSlots === 'string'
+      ? mergeVariants(classNamesOrSlots)
+      : Object.entries(
+          classNamesOrSlots as Record<
+            string,
+            (slotProps?: TVProps<V, S, EV, ES>) => string
+          >,
+        ).reduce(
+          (
+            acc: Record<string, (slotProps?: TVProps<V, S, EV, ES>) => string>,
+            [slot, callback],
+          ) => {
+            acc[slot] = (slotProps) => mergeVariants(callback(slotProps));
 
-    return typeof resultReturn === 'string'
-      ? mergeVariants(resultReturn)
-      : Object.entries<TailwindVariantFunction>(resultReturn).reduce<
-          Record<string, TailwindVariantFunction>
-        >((acc, [slot, callback]) => {
-          acc[slot] = (...a) => mergeVariants(callback(...a));
+            return acc;
+          },
+          {},
+        );
+  }) as TVReturnType<V, S, B, EV, ES, E>;
 
-          return acc;
-        }, {});
-  };
-
-  return Object.assign(wrapper, result);
-};
+  return Object.assign(wrappedStateMachine, configuredStateMachine);
+}
 
 // Types copied from RAC due to not being exported
 type WithRef<T, E> = T & {
