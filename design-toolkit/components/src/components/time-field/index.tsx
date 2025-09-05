5c72765a58f6e52dd3f2298a9c020ff06941ae08
Natalie Yip <yip.natalie92@gmail.com>
2025-09-05 10:57:34 -0700

    feat: add time field skeleton

---
+++ design-toolkit/components/src/components/time-field/index.tsx
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+'use client';
+import 'client-only';
+import {
+  TimeField as AriaTimeField,
+  type TimeValue,
+  composeRenderProps,
+} from 'react-aria-components';
+import type { TimeFieldProps } from './types';
+
+// const { field, label, control, input, segment, description, error } =
+//   DateFieldStyles();
+
+export function TimeField<T extends TimeValue>({
+  classNames,
+  description: descriptionProp,
+  errorMessage: errorMessageProp,
+  label: labelProp,
+  size = 'medium',
+  shouldForceLeadingZeros = true,
+  isDisabled,
+  isInvalid: isInvalidProp,
+  isRequired,
+  ...rest
+}: TimeFieldProps<T>) {
+  const errorMessage = errorMessageProp || null; // Protect against empty string
+
+  return (
+    <AriaTimeField<T>
+      {...rest}
+      shouldForceLeadingZeros={shouldForceLeadingZeros}
+      isDisabled={isDisabled}
+      isInvalid={isInvalidProp || (errorMessage ? true : undefined)} // Leave uncontrolled if possible to fallback to validation state
+      isRequired={isRequired}
+      aria-label={labelProp}
+      data-size={size}
+    ></AriaTimeField>
+  );
+}
---
+++ design-toolkit/components/src/components/time-field/time-field.stories.tsx
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { TimeField } from './index';
+
+const meta: Meta<typeof TimeField> = {
+  title: 'Components/TimeField',
+  component: TimeField,
+  args: {
+    label: 'Label',
+    description: 'Format: d MMM yyyy',
+    errorMessage: '',
+    size: 'medium',
+    isDisabled: false,
+    isInvalid: false,
+    isRequired: true,
+  },
+  argTypes: {
+    size: {
+      control: 'select',
+      options: ['small', 'medium'],
+    },
+    granularity: {
+      control: 'select',
+      options: ['day', 'hour', 'minute', 'second'],
+    },
+  },
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof TimeField> = {
+  render: TimeField,
+};
+
+export const WithoutShortMonth: StoryObj<typeof TimeField> = {
+  args: {
+    ...Default.args,
+    description: 'Format: d MM yyyy',
+  },
+  render: TimeField,
+};
