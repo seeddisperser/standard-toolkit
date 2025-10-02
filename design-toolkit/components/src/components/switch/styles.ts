1cd7f17f1b12600bd144d18df76e18e4b000e3a5
Calvin Hutcheon <hello@calvin.ooo>
2025-10-02 10:32:55 -0700

    Places state styles within single strings

--- design-toolkit/components/src/components/switch/styles.ts
+++ design-toolkit/components/src/components/switch/styles.ts
@@ -17,38 +17,22 @@ export const SwitchStyles = tv({
     switch:
       'group/switch flex cursor-pointer items-center gap-s disabled:cursor-not-allowed',
     control: [
-      'flex rounded-round bg-transparent p-xxs outline outline-interactive before:mr-l before:block before:size-m before:rounded-full before:[background-color:var(--fg-primary-muted)]', 
-      'before:transition-[margin,background-color] before:duration-150 before:ease-out',
+      'flex rounded-round bg-transparent p-xxs outline outline-interactive before:mr-l before:block before:size-m before:rounded-full before:transition-[margin,background-color] before:duration-150 before:ease-out before:[background-color:var(--fg-primary-muted)]',
       //focus-visible
-      'group-enabled/switch:group-focus-visible/switch:bg-interactive-muted-hover', 
-      'group-enabled/switch:group-focus-visible/switch:outline-interactive-hover', 
-      'group-enabled/switch:group-focus-visible/switch:before:[background-color:var(--fg-primary-bold)]',
-      // hover
-      'group-enabled/switch:group-hover/switch:bg-interactive-muted-hover', 
-      'group-enabled/switch:group-hover/switch:outline-interactive-hover', 
-      'group-enabled/switch:group-hover/switch:before:[background-color:var(--fg-primary-bold)]',
-      // pressed
-      'group-enabled/switch:group-pressed/switch:bg-interactive-pressed', 
-      'group-enabled/switch:group-pressed/switch:outline-interactive-pressed',
-      'group-enabled/switch:group-pressed/switch:outline-interactive-pressed', 
-      'group-enabled/switch:group-pressed/switch:before:[background-color:var(--fg-primary-bold)]',
-
-      //Selected
-      'group-enabled/switch:group-selected/switch:outline-accent-primary-bold group-selected/switch:before:mr-0 group-selected/switch:before:ml-l group-enabled/switch:group-selected/switch:before:[background-color:var(--fg-accent-primary-bold)]',
-      // focus-visble
-      'group-enabled/switch:group-selected/switch:group-focus-visible/switch:bg-accent-primary-muted', 
-      'group-enabled/switch:group-selected/switch:group-focus-visible/switch:outline-interactive-hover', 
-      'group-enabled/switch:group-selected/switch:group-focus-visible/switch:before:[background-color:var(--fg-accent-primary-hover)]',
+      'group-enabled/switch:group-focus-visible/switch:bg-interactive-muted-hover group-enabled/switch:group-focus-visible/switch:outline-interactive-hover group-enabled/switch:group-focus-visible/switch:before:[background-color:var(--fg-primary-bold)]',
       //hover
-      'group-enabled/switch:group-selected/switch:group-hover/switch:bg-accent-primary-muted', 
-      'group-enabled/switch:group-selected/switch:group-hover/switch:outline-accent-primary-bold', 
-      'group-enabled/switch:group-selected/switch:group-hover/switch:before:[background-color:var(--fg-accent-primary-hover)]',
-      // pressed
-      'group-enabled/switch:group-selected/switch:group-pressed/switch:bg-accent-primary-pressed', 
-      'group-enabled/switch:group-selected/switch:group-pressed/switch:outline-accent-primary-pressed', 
-      'group-enabled/switch:group-selected/switch:group-pressed/switch:before:[background-color:var(--fg-accent-primary-pressed)]',
-
-      // disabled
+      'group-enabled/switch:group-hover/switch:bg-interactive-muted-hover group-enabled/switch:group-hover/switch:outline-interactive-hover group-enabled/switch:group-hover/switch:before:[background-color:var(--fg-primary-bold)]',
+      //pressed
+      'group-enabled/switch:group-pressed/switch:bg-interactive-pressed group-enabled/switch:group-pressed/switch:outline-interactive-pressed group-enabled/switch:group-pressed/switch:outline-interactive-pressed group-enabled/switch:group-pressed/switch:before:[background-color:var(--fg-primary-bold)]',
+      //selected
+      'group-enabled/switch:group-selected/switch:outline-accent-primary-bold group-selected/switch:before:mr-0 group-selected/switch:before:ml-l group-enabled/switch:group-selected/switch:before:[background-color:var(--fg-accent-primary-bold)]',
+      //focus-visble
+      'group-enabled/switch:group-selected/switch:group-focus-visible/switch:bg-accent-primary-muted group-enabled/switch:group-selected/switch:group-focus-visible/switch:outline-interactive-hover group-enabled/switch:group-selected/switch:group-focus-visible/switch:before:[background-color:var(--fg-accent-primary-hover)]',
+      //hover
+      'group-enabled/switch:group-selected/switch:group-hover/switch:bg-accent-primary-muted group-enabled/switch:group-selected/switch:group-hover/switch:outline-accent-primary-bold group-enabled/switch:group-selected/switch:group-hover/switch:before:[background-color:var(--fg-accent-primary-hover)]',
+      //pressed
+      'group-enabled/switch:group-selected/switch:group-pressed/switch:bg-accent-primary-pressed group-enabled/switch:group-selected/switch:group-pressed/switch:outline-accent-primary-pressed group-enabled/switch:group-selected/switch:group-pressed/switch:before:[background-color:var(--fg-accent-primary-pressed)]',
+      //disabled
       'group-disabled/switch:bg-interactive-disabled group-disabled/switch:outline-interactive-disabled group-disabled/switch:before:[background-color:var(--fg-disabled)]',
     ],
     label: ['fg-primary-bold text-body-s', 'group-disabled/switch:fg-disabled'],
