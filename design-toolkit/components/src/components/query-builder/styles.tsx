32ce42f316bb5583e1f018370adf1af5aa541da0
Natalie Yip <yip.natalie92@gmail.com>
2025-10-06 15:49:16 -0700

    refactor: add styles file

---
+++ design-toolkit/components/src/components/query-builder/styles.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2025 Hypergiant Galactic Systems Inc. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+import { tv } from '@/lib/utils';
+
+export const QueryBuilderStylesDefaults = {} as const;
+
+export const QueryBuilderStyles = tv({
+  slots: {
+    queryBuilder: 'flex flex-col gap-s',
+    ruleGroup:
+      'group col-span-full flex flex-col gap-s p-s outline outline-info-bold rounded-medium',
+    header: 'flex gap-s',
+    // body: cn(
+    // 'group grid gap-x-s empty:hidden',
+    // showRuleLines
+    //     ? 'grid-cols-[10px_minmax(100px,_1fr)_min-content]'
+    //     : 'grid-cols-[minmax(100px,_1fr)_min-content]',
+    // ),
+    combinators: 'my-s',
+    addRule: '',
+    addGroup: '',
+    cloneRule: 'fg-info-bold hover:fg-info-hover',
+    cloneGroup: 'fg-info-bold hover:fg-info-hover',
+    removeGroup: '',
+    // rule: cn(
+    // 'flex gap-xs py-s',
+    // orientation === 'vertical'
+    //     ? 'flex-col'
+    //     : 'min-height-[50px] items-start',
+    // ),
+    fields: 'w-full',
+    operators: '',
+    value: 'w-full',
+    removeRule: '',
+    valid: '',
+    invalid: '',
+    disabled: '',
+    lockRule: 'fg-info-bold hover:fg-info-hover',
+    lockGroup: 'fg-info-bold hover:fg-info-hover',
+    valueSource: '',
+    valueListItem: '',
+  },
+  defaultVariants: QueryBuilderStylesDefaults,
+});
