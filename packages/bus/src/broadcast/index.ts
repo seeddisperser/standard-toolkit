7657da4a293a4d98c0cb9e34e5b2697ac62238ec
Ray Knight <array.knight+github@gmail.com>
2025-10-06 15:44:37 -0700

    feat: 🎸 add origin to event payload

--- packages/bus/src/broadcast/index.ts
+++ packages/bus/src/broadcast/index.ts
@@ -295,9 +295,10 @@ export class Broadcast<
     };
     const message = {
       type,
-      target: target === 'self' ? this.id : isUUID(target) ? target : undefined,
+      origin: this.id,
       payload,
-    } as Payload as P;
+      target: target === 'self' ? this.id : isUUID(target) ? target : undefined,
+    } as unknown as P;
 
     if (message.target !== this.id) {
       this.channel.postMessage(message);
